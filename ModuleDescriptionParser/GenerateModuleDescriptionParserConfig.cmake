ffdac9cc5d3fba3ba84782bbfcd3c6963ff570a1
Rafael Palomar <rafael.palomar@rr-research.no>
2019-09-28 21:50:53 +0200

    ENH: Generate configuration file for install ModuleDescriptionParser

--- ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
+++ ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
@@ -33,8 +33,8 @@ configure_file(
 
 # TODO Configure ModuleDescriptionParserConfig.cmake for the install tree.
 #
-#configure_file(
-#  ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserInstallConfig.cmake.in
-#  ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake
-#  @ONLY
-#  )
+configure_file(
+  ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserInstallConfig.cmake.in
+  ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake
+  @ONLY
+  )
